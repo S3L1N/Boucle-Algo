923c26768db1e33a099560b4d51c5b9e5e08982f
Selin <95339412+Selin68@users.noreply.github.com>
2022-01-13 10:11:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+/*Exercice 5
+Ecrire un algorithme qui demande un nombre de départ, et qui calcule la somme des entiers jusqu’à ce 
+nombre. Par exemple, si l’on entre 5, le programme doit calculer :
+1 + 2 + 3 + 4 + 5 = 15
+NB : on souhaite afficher uniquement le résultat, pas la décomposition du calcul*/
+let nb =5;
+let total= 0;
+for (i=1 ; i<=nb ; i++){
+    total+=i
+    console.log(total);
+}
+
+/*Ecrire un algorithme qui demande un nombre de départ, et qui calcule sa factorielle.
+NB : la factorielle de 8, notée 8 !, vaut
+1 x 2 x 3 x 4 x 5 x 6 x 7 x 8*/
+let nbr=8;
+let ttl=1;
+for (s=1;s<=nbr;s++){
+    ttl*=s
+}
+console.log("La factorielle de "+nbr+", notée "+nbr+" vaut "+ttl);
+
+/*Exercice 7
+Ecrire un algorithme qui demande successivement 20 nombres à l’utilisateur, et qui lui dise ensuite quel 
+était le plus grand parmi ces 20 nombres :
+Entrez le nombre numéro 1 : 12
+Entrez le nombre numéro 2 : 14 etc.
+Entrez le nombre numéro 20 : 6
+Le plus grand de ces nombres est : 14
+Modifiez ensuite l’algorithme pour que le programme affiche de surcroît en quelle position avait été saisie 
+ce nombre :
+C’était le nombre numéro 2*/
+let nmbre='';
+for (a=1;a<10;i++){
+
+}
+console.log()
+
